bacdf814c5930517effd581d001ea59b92fa9588
vinicius <viniciusvictorcorrea@gmail.com>
2023-10-24 12:03:03 -0300

    singup screen

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import Stack from './stack'
 import About from '../screens/About'
+import Singup from '../screens/Singup'
 
 export default function Routes(){
     const Drawer = createDrawerNavigator();
@@ -11,12 +12,10 @@ export default function Routes(){
 
             <Drawer.Screen name ='home' component = {Stack}/>
             <Drawer.Screen name ='sobre' component = {About} />
+            <Drawer.Screen name ='registre-se' component = {Singup} />
+
 
         </Drawer.Navigator>
-
-
-
-
 
     )
 }
